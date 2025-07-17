8c9041ea7ba378df58b9389749a4073466dc587f
owenparry-pb <73891819+owenparry-pb@users.noreply.github.com>
2025-07-17 12:18:17 -0400

    Update ItemUtilities.java
    refactor: remove deprecated getData() from ItemUtilities#getSignature
    
    - Eliminated all usage of ItemStack#getData() and related deprecated data access.
    - Now uses only ItemMeta and PotionMeta for creating item signatures.
    - Ensures compatibility with modern and future Spigot/Paper APIs.

--- src/main/java/dev/chaws/automaticinventory/utilities/ItemUtilities.java
+++ src/main/java/dev/chaws/automaticinventory/utilities/ItemUtilities.java
@@ -1,54 +1,37 @@
 package dev.chaws.automaticinventory.utilities;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 
 public class ItemUtilities {
-	public static String getSignature(ItemStack stack) {
-		var signature = stack.getType().name();
-		if (stack.getMaxStackSize() > 1) {
-			// getData will not actually be removed according to the spigot forums
-            //noinspection removal
-            var data = stack.getData();
-			if (data != null) {
-				// getData will not actually be removed according to the spigot forums
-				//noinspection deprecation
-				signature += "." + String.valueOf(data.getData());
-			}
-		}
+    public static String getSignature(ItemStack stack) {
+        StringBuilder signature = new StringBuilder(stack.getType().name());
 
-		var meta = stack.getItemMeta();
-		if (meta != null && meta.hasDisplayName()) {
-			// Append the name of the item is there is a custom name given to it
-			signature += "." + meta.displayName();
-		}
+        // Append custom display name if present
+        ItemMeta meta = stack.getItemMeta();
+        if (meta != null && meta.hasDisplayName()) {
+            signature.append(".").append(meta.displayName());
+        }
 
-		//differentiate potion types. Original credit to pugabyte: https://github.com/Pugabyte/AutomaticInventory/commit/01bbdbfa0ea1bc7dc397fc8a8ff625f3f22e1ed6
-		//Modified to use PotionType instead of PotionEffectType in signature
-		if (stack.getType().toString().toLowerCase().contains("potion")) {
-			var itemMeta = stack.getItemMeta();
-			if (itemMeta == null) {
-				return signature;
-			}
+        // Differentiate potion types
+        if (stack.getType().toString().toLowerCase().contains("potion")) {
+            if (meta instanceof PotionMeta potionMeta) {
+                var potionType = potionMeta.getBasePotionType();
+                if (potionType != null) {
+                    signature.append(".").append(potionType);
+                    if (potionType.isExtendable()) {
+                        signature.append(".extendable");
+                    }
+                    if (potionType.isUpgradeable()) {
+                        signature.append(".upgradable");
+                    }
+                }
+            }
+        }
 
-			if (!(itemMeta instanceof PotionMeta potionMeta)) {
-				return signature;
-			}
+        // Optionally, append additional meta info for other item types here
 
-			var potionType = potionMeta.getBasePotionType();
-			if (potionType == null) {
-				return signature;
-			}
-
-			signature += "." + potionType;
-			if (potionType.isExtendable()) {
-				signature += ".extendable";
-			}
-			if (potionType.isUpgradeable()) {
-				signature += ".upgradable";
-			}
-		}
-
-		return signature;
-	}
+        return signature.toString();
+    }
 }
